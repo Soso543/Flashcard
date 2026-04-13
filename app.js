f2e7b12734c9107feef4c5834cc6b562545f44a8
Soso543 <kankasemwong@gmail.com>
2026-04-13 11:09:38 +0700

    fit q2

--- app.js
+++ app.js
@@ -283,7 +283,6 @@ function loadCurrentRevisionCard() {
     if (card.image && card.image.trim() !== "") {
         reviseImage.src = card.image;
         reviseImage.style.display = 'block';
-        reviseQuestion.style.fontSize = "1.2rem"
     } else {
         reviseImage.src = "";
         reviseImage.style.display = 'none';
